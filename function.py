e697bc69264ab0dec2ffaa28a14421772cf7d8a6
user.name: "JuTeLabs <user.email: "julietech3@gmail.com>
2024-04-24 12:20:55 +0100

    functions

---
+++ function.py
@@ -0,0 +1,8 @@
+inbuilt_functions = "functions that come with python as default"
+#print(inbuilt_functions)
+
+# Hard-coded list of random numbers
+random_numbers = [23, 58, 94, 17, 36, 72, 89, 45, 66, 28]
+#print(max(random_numbers))
+floats = [1.4756, 2.99, 2.103]
+print(round(floats[2]))
--- list.py
+++ list.py
@@ -1,2 +1,4 @@
-names = ["monday", "Chimaobi", "Julius"]
-print(names)
+#names = ["monday", "Chimaobi", "Julius"]
+#print(names)
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+areas[-1][1]
